9220d66012f246f3c1c7d4bd24ac0fe1e62c9fea
rmoita <rmoita@Tsunami>
2018-01-28 21:03:56 +0100

    Cucumber report HTML format

--- spring-boot-example/src/test/java/com/moita/sbe/controller/cucumber/ItemRestControllerRunner.java
+++ spring-boot-example/src/test/java/com/moita/sbe/controller/cucumber/ItemRestControllerRunner.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/features",
+                 plugin = { "pretty", "html:target/cucumber" })
 public class ItemRestControllerRunner {
 
 }
