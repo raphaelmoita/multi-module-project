a6b8203e2c184b5f8f9db8269e87128b9534835a
Raphael Moita <raphael.moita@gmail.com>
2018-03-05 21:55:11 +0100

    Remove garbage

--- spring-boot-example/src/main/java/org/moita/sbe/jms/config/P2PConfig.java
+++
@@ -1,7 +0,0 @@
-package org.moita.sbe.jms.config;
-
-public final class P2PConfig {
-
-	public static final String QUEUE = "order-queue";
-	
-}
--- spring-boot-example/src/main/java/org/moita/sbe/jms/config/TopicConfig.java
+++
@@ -1,7 +0,0 @@
-package org.moita.sbe.jms.config;
-
-public final class TopicConfig {
-
-	public static final String TOPIC = "order-topic";
-	
-}
